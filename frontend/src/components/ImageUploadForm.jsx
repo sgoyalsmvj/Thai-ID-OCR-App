1a6fdf5091eb90796694575d4f2cbe3ac35d1c68
sgoyalsmvj <sgoyalsmvj@gmail.com>
2023-12-24 12:30:42 +0530

    Added alert message for response from backend

--- frontend/src/components/ImageUploadForm.jsx
+++ frontend/src/components/ImageUploadForm.jsx
@@ -1,43 +1,50 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const ImageUploadForm = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+  };
 
-    const [selectedImage,setSelectedImage] = useState(null);
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        setSelectedImage(file);
-      };
-    
-    const handleSubmit = async(event) => {
-        event.preventDefault();
-        if(!selectedImage){
-            alert('Please select an image');
-        }
-        try{
-            const formData = new FormData();
-            formData.append('file',selectedImage);
-            const response = await axios.post('/api/uploadImage',formData,{
-                headers:{
-                    'Content-Type':'multipart/form-data',
-                },
-            });
-        }
-        catch(error){
-            console.error('Error uploading file:',error);
-        }
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!selectedImage) {
+      alert("Please select an image");
     }
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedImage);
+      const response = await axios.post("/api/uploadImage", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (response.status === 200) {
+        alert("File uploaded successfully!");
+        // You can reset the form or perform other actions after successful upload
+      } else {
+        alert("File upload failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
   return (
     <div>
-        <h1>Upload ID card</h1>
-        <form action="/api/uploadImage" method='post' onSubmit={handleSubmit}> 
-            {selectedImage? <img alt='uploaded image' src ={URL.createObjectURL(selectedImage)} /> : <p>Select an image</p>}
-            <input type="file" onChange={handleFileChange}/>
-            <button type='submit'>Upload</button>
-        </form>
+      <h1>Upload ID card</h1>
+      <form action="/api/uploadImage" method="post" onSubmit={handleSubmit}>
+        {selectedImage ? (
+          <img alt="uploaded image" src={URL.createObjectURL(selectedImage)} />
+        ) : (
+          <p>Select an image</p>
+        )}
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ImageUploadForm
+export default ImageUploadForm;
