6e9253734cbfaea0599ab28596e07b878c5af940
sgoyalsmvj <sgoyalsmvj@gmail.com>
2023-12-24 12:43:45 +0530

    post route created ,able to fetch uploaded image to the backend ,  cross site connection established using proxy in vite.config.js

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const multer = require("multer");
 require("dotenv").config();
+
+app.post('/api/uploadImage',(req,res)=>{
+    console.log(req);
+})
+
+app.listen(3000,()=>{
+    console.log('hey i am listening at port 3000');
+})
