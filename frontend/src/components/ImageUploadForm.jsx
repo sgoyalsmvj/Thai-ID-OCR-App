9bc70c794d408d2af984914502b32658d6934c46
sgoyalsmvj <sgoyalsmvj@gmail.com>
2023-12-24 12:29:46 +0530

    feat: add file upload functionality to React component
    Implemented file upload functionality in the FileUploadForm component. The selected file is sent to the backend using a POST request, and the response is handled accordingly. This includes displaying success or failure alerts. The code uses the FormData API for handling file uploads.

--- frontend/src/components/ImageUploadForm.jsx
+++ frontend/src/components/ImageUploadForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const ImageUploadForm = () => {
 
@@ -7,16 +8,30 @@ const ImageUploadForm = () => {
         const file = event.target.files[0];
         setSelectedImage(file);
       };
-    const handleSubmit = (event) => {
+    
+    const handleSubmit = async(event) => {
         event.preventDefault();
         if(!selectedImage){
             alert('Please select an image');
         }
+        try{
+            const formData = new FormData();
+            formData.append('file',selectedImage);
+            const response = await axios.post('/api/uploadImage',formData,{
+                headers:{
+                    'Content-Type':'multipart/form-data',
+                },
+            });
+        }
+        catch(error){
+            console.error('Error uploading file:',error);
+        }
+
     }
   return (
     <div>
         <h1>Upload ID card</h1>
-        <form action="/uploadImage" method='post' onSubmit={handleSubmit}> 
+        <form action="/api/uploadImage" method='post' onSubmit={handleSubmit}> 
             {selectedImage? <img alt='uploaded image' src ={URL.createObjectURL(selectedImage)} /> : <p>Select an image</p>}
             <input type="file" onChange={handleFileChange}/>
             <button type='submit'>Upload</button>
