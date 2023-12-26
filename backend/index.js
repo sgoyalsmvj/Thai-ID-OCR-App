c484516aeca61ec9453a57b7d05985119d9eb080
sgoyalsmvj <sgoyalsmvj@gmail.com>
2023-12-26 12:58:12 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,13 @@ const detectText = require("./utils/ocrUtils");
 const Card = require("./models/Card");
 const cors =require('cors');
 // Connecting to DB
-app.use(cors());
+// const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 mongoose
   .connect(process.env.MONGODB_URI, {
